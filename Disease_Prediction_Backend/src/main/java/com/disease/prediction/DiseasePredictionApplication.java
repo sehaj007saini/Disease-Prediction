d38c8ac0801f6ba041473290d28c8ac1571742d4
Sehaj007Saini <sehajsainif5@gmail.com>
2026-09-13 09:58:00 +0530

    Fix: Add RestTemplate bean for Gemini service

--- Disease_Prediction_Backend/src/main/java/com/disease/prediction/DiseasePredictionApplication.java
+++ Disease_Prediction_Backend/src/main/java/com/disease/prediction/DiseasePredictionApplication.java
@@ -2,11 +2,18 @@ package com.disease.prediction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DiseasePredictionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiseasePredictionApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
